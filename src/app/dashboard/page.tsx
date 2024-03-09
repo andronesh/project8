c029cd2141a5376c41bcca6d8b01f477b8321be7
Andrii Shalankevych <shalankevych@gmail.com>
2024-03-09 18:38:06 +0200

    Add MagikLink auth using supabase

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,12 +4,7 @@ export default function Home() {
   return (
     <div className="main-h-screen bg-gray-900 text-gray-300">
       <div className="container mx-auto p-6 sm:p-12">
-        <h1 className="text-lg md:text-xl text-white mb-6">
-          Hello, please Log In to continue
-        </h1>
-        <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-          <AuthForm />
-        </div>
+        <h1 className="text-lg md:text-xl text-white mb-6">Hello</h1>
       </div>
     </div>
   );
