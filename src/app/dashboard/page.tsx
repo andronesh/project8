e1da9a5a2869b57208de456540e3cc80e78717e8
Andrii Shalankevych <shalankevych@gmail.com>
2024-03-10 14:05:44 +0200

    Add Sign Out button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,20 @@
-import AuthForm from "@/components/AuthForm";
-
 export default function Home() {
   return (
     <div className="main-h-screen bg-gray-900 text-gray-300">
       <div className="container mx-auto p-6 sm:p-12">
-        <h1 className="text-lg md:text-xl text-white mb-6">Hello</h1>
+        <div className="flex justify-between items-start">
+          <h1 className="text-4xl md:text-4xl text-white mb-6">
+            Project<b>8</b>
+          </h1>
+          <form action="/auth/signout" method="post">
+            <button
+              type="submit"
+              className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Sign Out
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
