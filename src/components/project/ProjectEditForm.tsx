214d4e09a3d523c3f3cdc747e2d333a5a7046b79
Andrii Shalankevych <shalankevych@gmail.com>
2024-03-11 08:36:56 +0200

    Restyle project edit form

--- src/components/project/ProjectEditForm.tsx
+++ src/components/project/ProjectEditForm.tsx
@@ -27,56 +27,66 @@ export default function ProjectEditForm({ project, onCancel }: Props) {
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
   return (
-    <form action={project ? updateProject : createProject} className="m6-6">
-      <input
-        type="text"
-        id="id"
-        name="id"
-        value={formData.id}
-        className="hidden"
-      />
-      <div className="mb-4">
-        <label htmlFor="name" className="block text-gray-300 mb-2">
-          Name
-        </label>
+    <div className="w-full max-w-sm p-4 pt-0 bg-white border border-gray-200 dark:bg-gray-800 dark:border-gray-700 rounded-lg shadow">
+      <form
+        className="space-y-4"
+        action={project ? updateProject : createProject}
+      >
         <input
           type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          className="w-full p-2 rounded bg-gray-800 text-white border border-gray-700 focus:border-blue-500"
+          id="id"
+          name="id"
+          value={formData.id}
+          className="hidden"
         />
-      </div>
-      <div className="flex flex-row justify-evenly">
-        <div className="flex">
-          <button
-            type="reset"
-            className="w-full hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-            onClick={onCancel}
+        <div>
+          <label
+            htmlFor="name"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Cancel
-          </button>
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            placeholder="Bucket List"
+            required
+          />
         </div>
-        {project && (
+        <div className="flex flex-row justify-evenly">
           <div className="flex">
             <button
               type="reset"
-              className="w-full text-red-400 hover:bg-red-700 hover:text-white font-bold py-2 px-4 rounded"
+              className="w-full hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+              onClick={onCancel}
             >
-              Delete
+              Cancel
             </button>
           </div>
-        )}
-        <div className="flex">
-          <button
-            type="submit"
-            className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Save
-          </button>
+          {project && (
+            <div className="flex">
+              <button
+                type="reset"
+                className="w-full text-red-400 hover:bg-red-700 hover:text-white font-bold py-2 px-4 rounded"
+              >
+                Delete
+              </button>
+            </div>
+          )}
+          <div className="flex">
+            <button
+              type="submit"
+              className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Save
+            </button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
--- src/components/project/ProjectsPanel.tsx
+++ src/components/project/ProjectsPanel.tsx
@@ -25,7 +25,7 @@ export default function ProjectsPanel({ projects }: Props) {
     <div className="container mx-auto">
       <div className="flex flex-row flex-wrap">
         <aside className="w-full sm:w-1/3 md:w-1/4 px-2">
-          <div className="sticky top-0 p-4 w-full">
+          <div className="sticky top-0 w-full">
             <div
               className="flex items-center justify-center mb-2 p-4 rounded-lg bg-gray-50 dark:bg-gray-800 hover:cursor-pointer hover:bg-gray-700"
               onClick={() => initProjectCreation()}
@@ -66,7 +66,7 @@ export default function ProjectsPanel({ projects }: Props) {
             ))}
           </div>
         </aside>
-        <main role="main" className="w-full sm:w-2/3 md:w-3/4 pt-1 px-2">
+        <main role="main" className="w-full sm:w-2/3 md:w-3/4 px-2">
           {editFormVisible && (
             <ProjectEditForm
               project={selectedProject}
