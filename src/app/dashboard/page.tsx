b71c5d84068f79a83db51feca05b0b9394c22aff
Andrii Shalankevych <shalankevych@gmail.com>
2024-03-12 15:48:23 +0200

    Add sidebar and move projects list to it

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -19,22 +19,7 @@ export default async function Dashboard() {
   // const projects: Project[] = new Array<Project>();
 
   return (
-    <div className="h-full bg-gray-900 text-gray-300">
-      <div className="container mx-auto p-6 pb-0">
-        <div className="flex justify-between items-start">
-          <h1 className="text-4xl md:text-4xl text-white mb-6">
-            Project<b>8</b>
-          </h1>
-          <form action="/auth/signout" method="post">
-            <button
-              type="submit"
-              className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Sign Out
-            </button>
-          </form>
-        </div>
-      </div>
+    <div className=" bg-gray-900 text-gray-300">
       <ProjectsPanel projects={data !== null ? (data as Project[]) : []} />
     </div>
   );
--- src/components/project/ProjectsPanel.tsx
+++ src/components/project/ProjectsPanel.tsx
@@ -23,76 +23,159 @@ export default function ProjectsPanel({ projects }: Props) {
   };
 
   return (
-    <div className="container mx-auto">
-      <div className="flex flex-row flex-wrap">
-        <aside className="w-full sm:w-1/3 md:w-1/4 px-2">
-          <div className="sticky top-0 w-full">
-            <div
-              className="flex items-center justify-center mb-2 p-4 rounded-lg bg-gray-50 dark:bg-gray-800 hover:cursor-pointer hover:bg-gray-700"
-              onClick={() => initProjectCreation()}
-            >
-              <p className="text-2xl text-gray-400 dark:text-gray-500">
+    <>
+      <button
+        data-drawer-target="default-sidebar"
+        data-drawer-toggle="default-sidebar"
+        aria-controls="default-sidebar"
+        type="button"
+        className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+      >
+        <span className="sr-only">Open sidebar</span>
+        <svg
+          className="w-6 h-6"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            clip-rule="evenodd"
+            fill-rule="evenodd"
+            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+          ></path>
+        </svg>
+      </button>
+
+      <aside
+        id="default-sidebar"
+        className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+          <ul className="space-y-2 font-medium">
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
                 <svg
-                  className="w-3.5 h-3.5"
+                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 18 18"
+                  fill="currentColor"
+                  viewBox="0 0 22 21"
                 >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 1v16M1 9h16"
-                  />
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
                 </svg>
-              </p>
-            </div>
-            {projects?.map((project: Project) => (
-              <div
-                key={project.id}
-                className={`flex justify-between mb-2 p-4 ${
-                  selectedProject?.id === project.id
-                    ? "bg-blue-900"
-                    : "bg-gray-800 hover:bg-gray-700"
-                } rounded-lg shadow hover:cursor-pointer`}
-                onClick={() => {
-                  selectProject(project);
-                }}
-              >
-                <h2>{project.name}</h2>
+                <span className="ms-3">Dashboard</span>
+              </a>
+            </li>
+            <li>
+              <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                  onClick={() => setEditFormVisible(true)}
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
-                  />
+                  <path d="M32 32H480c17.7 0 32 14.3 32 32V96c0 17.7-14.3 32-32 32H32C14.3 128 0 113.7 0 96V64C0 46.3 14.3 32 32 32zm0 128H480V416c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V160zm128 80c0 8.8 7.2 16 16 16H336c8.8 0 16-7.2 16-16s-7.2-16-16-16H176c-8.8 0-16 7.2-16 16z" />
                 </svg>
+
+                <span className="flex-1 ms-3 whitespace-nowrap">Projects</span>
+                <span
+                  onClick={() => initProjectCreation()}
+                  className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium bg-gray-50 dark:bg-gray-800 hover:cursor-pointer hover:bg-gray-700"
+                >
+                  <p className="text-2xl text-gray-400 dark:text-gray-500">
+                    <svg
+                      className="w-3.5 h-3.5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 18 18"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9 1v16M1 9h16"
+                      />
+                    </svg>
+                  </p>
+                </span>
               </div>
-            ))}
-          </div>
-        </aside>
-        <main role="main" className="w-full sm:w-2/3 md:w-3/4 px-2">
-          {editFormVisible && (
-            <ProjectEditForm
-              project={selectedProject}
-              onCancel={cancelEditForm}
-            />
-          )}
-          {selectedProject && !editFormVisible && (
-            <IssuesPanel project={selectedProject} />
-          )}
-        </main>
+              <ul className="py-2 space-y-2">
+                {projects?.map((project: Project) => (
+                  <li>
+                    <div
+                      // href={`/projects/${project.id}`}
+                      key={project.id}
+                      onClick={() => {
+                        selectProject(project);
+                      }}
+                      className={`flex items-center justify-between w-full p-2 transition duration-75 rounded-lg pl-11 group  text-white  hover:cursor-pointer ${
+                        selectedProject?.id === project.id
+                          ? "bg-blue-900 hover:bg-blue-900"
+                          : "bg-gray-800 hover:bg-gray-700"
+                      }`}
+                    >
+                      <h2>{project.name}</h2>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                        fill="currentColor"
+                        aria-hidden="true"
+                        className="flex-shrink-0 w-4 h-4 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                        onClick={() => setEditFormVisible(true)}
+                      >
+                        <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
+                      </svg>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          </ul>
+          <ul className="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
+            <li>
+              <form action="/auth/signout" method="post">
+                <button
+                  type="submit"
+                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  >
+                    <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z" />
+                  </svg>
+                  <span className="flex-1 ms-3 whitespace-nowrap">
+                    Sign Out
+                  </span>
+                </button>
+              </form>
+            </li>
+          </ul>
+        </div>
+      </aside>
+      <div className="p-4 sm:ml-64 h-screen">
+        {editFormVisible && (
+          <ProjectEditForm
+            project={selectedProject}
+            onCancel={cancelEditForm}
+          />
+        )}
+        {selectedProject && !editFormVisible && (
+          <IssuesPanel project={selectedProject} />
+        )}
       </div>
-    </div>
+    </>
   );
 }
