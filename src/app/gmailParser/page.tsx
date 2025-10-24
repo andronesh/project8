161b01774a3b3c1e8b302316a47d4f00444e24b6
Andrii Shalankevych <shalankevych@gmail.com>
2025-10-25 02:48:07 +0300

    Fix GMail parser page loading

--- src/app/gmailParser/page.tsx
+++ src/app/gmailParser/page.tsx
@@ -1,5 +1,11 @@
+import LoadingSpinner from "@/components/common/LoadingSpinner";
 import EmailsPanel from "@/components/emails/EmailsPanel";
+import { Suspense } from "react";
 
 export default async function GMailParserPage() {
-	return <EmailsPanel />;
+	return (
+		<Suspense fallback={<LoadingSpinner className="flex w-full justify-around px-16 py-8" />}>
+			<EmailsPanel />
+		</Suspense>
+	);
 }
