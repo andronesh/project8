f18ec7589893158626df6f4494b4b47884c7b94d
Andrii Shalankevych <shalankevych@gmail.com>
2026-01-06 16:41:22 +0200

    fix: API URL env var should be visible for client

--- project8_nextjs/src/clients/apiClient.ts
+++ project8_nextjs/src/clients/apiClient.ts
@@ -1,6 +1,6 @@
 import { treaty } from "@elysiajs/eden";
 import { ServerApp } from "@/app/api/[[...slugs]]/route";
 
-const appBaseUrl = process.env.APP_URL_BASE || "localhost:3000";
+const appBaseUrl = process.env.NEXT_PUBLIC_APP_URL_BASE || "localhost:3000";
 
 export const apiClient = treaty<ServerApp>(appBaseUrl);
